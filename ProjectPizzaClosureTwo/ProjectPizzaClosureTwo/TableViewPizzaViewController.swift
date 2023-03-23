4ecf6a5e0c65a4ce4328cff7fe7577d76179c6bf
Mirian Santana <miriansantana93@gmail.com>
2023-03-23 02:10:02 -0300

    Added arrayPizza and dataSource

--- ProjectPizzaClosureTwo/ProjectPizzaClosureTwo/TableViewPizzaViewController.swift
+++ ProjectPizzaClosureTwo/ProjectPizzaClosureTwo/TableViewPizzaViewController.swift
@@ -11,21 +11,32 @@ class TableViewPizzaViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var arrayPizza: Pizza?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+    }
+
+}
+extension TableViewPizzaViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayPizza?.count ?? 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell {
+            
+            cell.setupPizza(pizza: arrayPizza?[indexPath.row])
+            
+            return cell
+        }
+        
+        return UITableViewCell()
     }
-    */
-
+    
+    
+    
 }
