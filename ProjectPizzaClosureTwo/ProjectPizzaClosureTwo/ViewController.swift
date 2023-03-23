7e0431bcb3e1d06bb147b03b08388c072d3fa377
Mirian Santana <miriansantana93@gmail.com>
2023-03-23 01:46:15 -0300

    Added pod Alamofire and pod SDWebImage. Button conected to class ViewController

---
+++ ProjectPizzaClosureTwo/ProjectPizzaClosureTwo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ProjectPizzaClosureTwo
+//
+//  Created by Mirian Santana on 23/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btStart(_ sender: Any) {
+    }
+    
+}
+
